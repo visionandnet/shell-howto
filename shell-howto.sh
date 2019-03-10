f0569c30a50ed3081fcf98b8d9f11ea04336d600
cyber4nt <48153372+visionandnet@users.noreply.github.com>
2019-03-10 02:03:28 +0100

    Create shell-howto.sh

---
+++ shell-howto.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#HelloWorld Skript
+echo "##### example 1 #####"
+echo "HelloWorld"
+echo ""
+
+
+echo "##### exmaple 2 #####"
+printf 'HelloWorld\n%.0s' {1..5}
+echo ""
+
+
+echo "##### exmaple 3 #####"
+for i in {1..4}
+do
+    echo "HelloWorld"
+done
+echo ""
+
+
+echo "##### exmaple 4 #####"
+n=7
+for (( c=1; c<=n; c++)) 
+do
+	echo "HelloWorld" 
+done
